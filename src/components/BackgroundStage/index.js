18ae44372a6324bd255d9c196921e53bbcbab42c
Joshua Byrd <phocks@gmail.com>
2020-05-07 13:50:28 +1000

    Testing animations

--- src/components/BackgroundStage/index.js
+++ src/components/BackgroundStage/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import styles from './styles.scss';
+import "./keyshapejs.js";
 
-import testsvg from "./animation-test.svg"
+import React from "react";
+import SVG from "react-inlinesvg";
+import { Keyshape } from "react-keyshape";
 
+import styles from "./styles.scss";
 
+import testsvg from "./animation-test.svg";
 
 export default props => {
   return (
     <div className={styles.root}>
-      <img src={testsvg} />
+      <Keyshape
+        svg={testsvg}
+        callback={(player, keyshapeElement) => {
+          setTimeout(() => {
+            player.pause();
+          }, 8000); // Pauses the animation after 8s.
+        }}
+      />
     </div>
   );
-}
+};
