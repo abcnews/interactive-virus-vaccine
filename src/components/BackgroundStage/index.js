b94ece1dfa758b95fb7cbff5037c76dfc8b3a78b
Joshua Byrd <phocks@gmail.com>
2020-05-21 14:32:38 +1000

    Testing separate

--- src/components/BackgroundStage/index.js
+++ src/components/BackgroundStage/index.js
@@ -7,16 +7,51 @@ import styles from "./styles.scss";
 import testsvg from "./images/vax.svg";
 import virus from "./images/virus.svg";
 
+import vaccine1 from "./testing/vaccine1-1.svg";
+import vaccine2 from "./testing/vaccine1-2.svg";
+import vaccine3 from "./testing/vaccine1-3.svg";
+import vaccine4 from "./testing/vaccine1-4.svg";
+import vaccine5 from "./testing/vaccine1-5.svg";
+import vaccine6 from "./testing/vaccine1-6.svg";
+import vaccine7 from "./testing/vaccine1-7.svg";
+import vaccine8 from "./testing/vaccine1-8.svg";
+import vaccine9 from "./testing/vaccine1-9.svg";
+
 export default props => {
   useEffect(() => {
     console.log(props.storyState);
   });
 
+  const getAnimation = key => {
+    switch (props.storyState) {
+      case "one":
+        return vaccine1;
+      case "two":
+        return vaccine2;
+      case "three":
+        return vaccine3;
+      case "four":
+        return vaccine4;
+      case "five":
+        return vaccine5;
+      case "six":
+        return vaccine6;
+      case "seven":
+        return vaccine7;
+      case "eight":
+        return vaccine8;
+      case "nine":
+        return vaccine9;
+      default:
+        return vaccine1;
+    }
+  };
+
   return (
     <div className={styles.root}>
       <div className={styles.svgContainer}>
         <SVG
-          src={props.storyState === "virus" ? virus : testsvg}
+          src={props.storyState === "virus" ? virus : getAnimation()}
           // uniquifyIDs={true}
           // uniqueHash={"unique"}
         />
